cbd3d2aa007c9b283e8b2ee9b39c0e5a4fc16cd9
woolosh <joshuapmorales@gmail.com>
2021-01-27 00:55:10 -0600

    commit tree 732b09dcfbb3f18e2b58ca4051b104df9dc0f1bb

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 require 'bundler'
 Bundler.require
 
-require_relative '../lib/student'
+require_relative '../lib/student.rb'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,47 @@
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
   
-end
+  attr_reader :name, :grade, :id
+  
+    def initialize(name, grade, id=nil)
+      @id = id
+      @name = name
+      @grade = grade
+    end
+
+    def self.create_table
+      sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students
+      (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)
+      SQL
+      DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+      sql = "DROP TABLE students;"
+      DB[:conn].execute(sql)
+    end
+
+    def self.create(name:, grade:)
+      student = Student.new(name, grade)
+      student.save
+      student
+    end
+
+    def save
+      sql = <<-SQL
+        INSERT INTO students (name, grade) 
+        VALUES (?, ?)
+      SQL
+  
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  
+    end
+
+    # Remember, you can access your database connection anywhere in this class with
+    # DB[:conn]  
+    
+  end
+  
+  new_student = Student.new("Joshua", 12, 1)
